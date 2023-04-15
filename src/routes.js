01c7038006f769b489810acd4fdda91d5137a04c
Jośe Xavier <nettoxavier23@gmail.com>
2023-04-15 20:53:56 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,68 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+
+const database = new Database()
+
+export const routes = [
+    { 
+        "method": 'GET',
+        "path": buildRoutePath("/users"),      
+        handler: (req, res) => {
+            const { search } = req.query
+
+            const users = database.select('users', search ? { 
+                fullName: search,
+                email: search
+            }: null)
+    
+            return res.end(JSON.stringify(users));
+        },
+    },
+    { 
+        "method": 'POST',
+        "path": buildRoutePath("/users"),
+        handler: (req, res) => {
+            const { fullName, email } = req.body
+
+            const user = {
+                id: randomUUID(),
+                fullName,
+                email,       
+            }
+
+            database.insert('users', user)
+
+            return  res.writeHead(200).end()
+        },
+    },
+    { 
+        "method": 'DELETE',
+        "path": buildRoutePath("/users/:id"),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
+        },
+    },
+    { 
+        "method": 'PUT',
+        "path": buildRoutePath("/users/:id"),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            const { fullName, email } = req.body
+
+            database.update('users', id,{
+                fullName,
+                email
+            })
+
+
+
+            return res.writeHead(204).end()
+        },
+    },
+]
